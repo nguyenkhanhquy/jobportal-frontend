d840bdb806309e3de251dbd616e7250b9b6571dc
NguyenDink <145578651+NguyenDink@users.noreply.github.com>
2024-11-13 04:02:14 +0700

    add: Register Page

---
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,36 @@
+import RegisterForm from "../../components/forms/RegisterForm/RegisterForm";
+import Public_layout_cover from "/images/public_layout_cover.jpg";
+
+const RegisterPage = () => {
+    return (
+        <div className="flex h-screen">
+            {/* Bên trái: Form đăng ký chiếm 5/9 */}
+            <div
+                className="md:w-5/9 flex h-screen w-full flex-col items-center justify-center overflow-y-scroll bg-white p-8"
+                style={{
+                    scrollbarWidth: "none", // Ẩn thanh cuộn cho Firefox
+                    msOverflowStyle: "none", // Ẩn thanh cuộn cho IE và Edge
+                }}
+            >
+                <style>
+                    {`
+                    /* Ẩn thanh cuộn cho Chrome, Safari, Edge */
+                    .no-scrollbar::-webkit-scrollbar {
+                        display: none;
+                    }
+                    `}
+                </style>
+                <div className="no-scrollbar w-full max-w-md">
+                    <RegisterForm />
+                </div>
+            </div>
+
+            {/* Bên phải: Hình ảnh minh họa chiếm 4/9 */}
+            <div className="w-4/9 hidden h-screen md:flex">
+                <img src={Public_layout_cover} alt="Banner" className="h-full w-full object-cover" />
+            </div>
+        </div>
+    );
+};
+
+export default RegisterPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            width: {
+                "5/9": "55.5555%", // 5/9 = 5 chia 9
+                "4/9": "44.4444%", // 4/9 = 4 chia 9
+            },
+        },
     },
     plugins: [],
 };
