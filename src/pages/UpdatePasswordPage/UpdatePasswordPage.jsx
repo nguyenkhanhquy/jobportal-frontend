ad777dff8103d83f31643595e69720e034e17774
NguyenDink <145578651+NguyenDink@users.noreply.github.com>
2024-11-17 16:43:26 +0700

    add: Update Password Form

--- src/pages/UpdatePasswordPage/UpdatePasswordPage.jsx
+++ src/pages/UpdatePasswordPage/UpdatePasswordPage.jsx
@@ -1,13 +1,12 @@
 import MainLayout from "../../layouts/MainLayout/MainLayout";
 import AccountLayout from "../../layouts/AccountLayout/AccountLayout";
-import EmptyBox from "../../components/box/EmptyBox";
+import UpdatePassWordForm from "../../components/forms/UpdatePasswordForm/UpdatePasswordForm";
 
 const UpdatePasswordPage = () => {
     return (
         <MainLayout title="Đổi mật khẩu">
             <AccountLayout>
-                <h2 className="mb-4 text-2xl font-semibold">Đổi mật khẩu</h2>
-                <EmptyBox />
+                <UpdatePassWordForm />
             </AccountLayout>
         </MainLayout>
     );
