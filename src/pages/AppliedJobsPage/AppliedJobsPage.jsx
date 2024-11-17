11bfe61b40a2984cb3903915f7eae1483c3595c9
NguyenDink <145578651+NguyenDink@users.noreply.github.com>
2024-11-17 17:49:45 +0700

    add: Applied Jobs GridView

--- src/pages/AppliedJobsPage/AppliedJobsPage.jsx
+++ src/pages/AppliedJobsPage/AppliedJobsPage.jsx
@@ -1,13 +1,12 @@
 import MainLayout from "../../layouts/MainLayout/MainLayout";
 import AccountLayout from "../../layouts/AccountLayout/AccountLayout";
-import EmptyBox from "../../components/box/EmptyBox";
+import AppliedJobsGridView from "../../components/gridview/AppliedJobsGridView/AppliedJobsGridView";
 
 const AppliedJobsPage = () => {
     return (
         <MainLayout title="Việc đã ứng tuyển">
             <AccountLayout>
-                <h2 className="mb-4 text-2xl font-semibold">Công việc đã ứng tuyển</h2>
-                <EmptyBox />
+                <AppliedJobsGridView />
             </AccountLayout>
         </MainLayout>
     );
