836b1938440d860a0f5ad3bf43eb2654658a933c
NguyenDink <145578651+NguyenDink@users.noreply.github.com>
2024-11-20 00:19:41 +0700

    update: New Jobs Section

--- src/components/section/Home/NewJobsSection.jsx
+++ src/components/section/Home/NewJobsSection.jsx
@@ -1,8 +1,11 @@
 import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom"; // Import useNavigate để điều hướng
 import HomeJobCard from "../../card/JobCard/HomeJobCard";
 import CircularProgress from "@mui/material/CircularProgress";
 
 const NewJobsSection = ({ jobData, loading }) => {
+    const navigate = useNavigate(); // Hook để điều hướng
+
     return (
         <div className="container mx-auto py-8">
             <h2 className="mb-8 text-center text-3xl font-bold text-green-600">Việc làm mới nhất</h2>
@@ -19,6 +22,16 @@ const NewJobsSection = ({ jobData, loading }) => {
                     ))}
                 </div>
             )}
+
+            {/* Nút "Xem thêm" */}
+            <div className="mt-8 flex justify-center">
+                <button
+                    onClick={() => navigate("/search")}
+                    className="rounded-lg bg-green-600 px-4 py-2 text-base font-semibold text-white hover:bg-green-700"
+                >
+                    Xem thêm...
+                </button>
+            </div>
         </div>
     );
 };
